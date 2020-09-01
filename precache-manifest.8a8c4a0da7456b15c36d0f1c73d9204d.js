403260f4a699e839b4ce64a4e5b7ee5293af6439
kaitlynning <kaitlynning88308@gmail.com>
2020-09-01 16:40:44 +0800

    Updates

---
+++ precache-manifest.8a8c4a0da7456b15c36d0f1c73d9204d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c7508f5581ba6bb85398a6f768085f",
+    "url": "/Solitaire/index.html"
+  },
+  {
+    "revision": "8111781b2b36a4b2cb41",
+    "url": "/Solitaire/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d197ad89ebadd4c98227",
+    "url": "/Solitaire/static/js/2.cd48967a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Solitaire/static/js/2.cd48967a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8111781b2b36a4b2cb41",
+    "url": "/Solitaire/static/js/main.d4f40b86.chunk.js"
+  },
+  {
+    "revision": "f05d9d754bcd1bed6d64",
+    "url": "/Solitaire/static/js/runtime-main.480e7b3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Solitaire/static/media/logo.5d5d9eef.svg"
+  }
+]);
